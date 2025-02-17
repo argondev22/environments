3db481f09483a3918636421668457bcd44cbf030
argon-dev22 <hyouga.programing@gmail.com>
2025-02-17 17:48:39 +0900

    config: add install session-manager-plugin

---
+++ install/aws.sh
@@ -0,0 +1,6 @@
+# Install AWS Session Manager Plugin
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac_arm64/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
+unzip sessionmanager-bundle.zip
+sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+## Confirm installation
+session-manager-plugin
