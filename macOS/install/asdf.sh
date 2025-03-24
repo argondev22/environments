54cc88c3278b05f79085e731d15c99b5b3e7695a
argon-dev22 <hyouga.programing@gmail.com>
2025-03-24 11:55:04 +0900

    config: asdfおよびTerraformのインストールスクリプトを追加

---
+++ macOS/install/asdf.sh
@@ -0,0 +1,2 @@
+brew install asdf
+echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
---
+++ macOS/install/terraform.sh
@@ -0,0 +1,5 @@
+# 前提条件: asdfがインストールされていること
+
+asdf plugin add terraform
+asdf install terraform latest
+asdf global terraform latest
