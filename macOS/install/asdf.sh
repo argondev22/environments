7bc71e34676aaa9639f5f2676d21ad5c5401db4e
argon-dev22 <hyouga.programing@gmail.com>
2025-03-26 03:25:51 +0900

    config: asdfのインストール方法をHomebrewからGitクローンに変更し、zshrcの設定を更新

--- macOS/install/asdf.sh
+++ macOS/install/asdf.sh
@@ -1,2 +1,4 @@
-brew install asdf
-echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+echo -e "\n. ~/.asdf/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
+echo -e "\n. fpath(${ASDF_DIR}/completions)" >> ${ZDOTDIR:-~}/.zshrc
